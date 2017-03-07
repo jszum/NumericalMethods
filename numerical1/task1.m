c4401e93a66f5a6cf0e14798be7d5e08f7f1bb1e
jszum <jaroslaw.szumega@gmail.com>
2017-03-08 00:43:40 +0100

    Direct Methods: Task 1 completed.

--- numerical1/task1.m
+++ numerical1/task1.m
@@ -2,6 +2,24 @@ A = [2 -1 0 0; -1 2 -1 0; 0 -1 2 -1; 0 0 -1 2];
 b = [0; 0; 0; 5];
 C = [A, b];
 
-C = exchange(C, 1, 2)
+# basic operations to achieve upper triangular form
+C = exchange(C, 1, 2);
 
-C(2,:) =C(2,:) + C(1,:)/2
+C(2,:) = C(2,:) + 2*C(1,:);
+
+C = exchange(C,2,3);
+C(3,:) = C(3,:) + 3*C(2,:);
+
+C = exchange(C,3,4);
+C(4,:) = C(4,:) + 4*C(3,:);
+
+
+#hand-made backsubstitution
+x4 = z = C(4,5)/C(4,4);
+x3 = w = (C(3,5) - C(3,4)*x4)/C(3,3);
+x2 = v = (C(2,5) - C(2,3)*x3 - C(2,4)*x4)/C(2,2);
+x1 = u = (C(1,5) - C(1,2)*x2 - C(1,3)*x3 - C(1,4)*x4)/C(1,1);
+u
+v 
+w 
+z
