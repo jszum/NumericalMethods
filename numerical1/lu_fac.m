d2c3431d6c4b62025699e1dbe7e1874046e017e4
jszum <jaroslaw.szumega@gmail.com>
2017-03-13 21:50:42 +0100

    Direct Methods: Task7 completed.

--- numerical1/lu_fac.m
+++ numerical1/lu_fac.m
@@ -1,3 +1,27 @@
 function [L, U] = lu_fac(A)
 
-[n, n] = size(A)
+[n, n] = size(A);
+
+L = eye(n);
+U = zeros(n,n)
+
+for j = 1:n
+  if (j == 1)
+    v(j:n) = A(j:n,j);
+    
+  else
+    #the elimination below won't work
+    #z = (A(1:j-1,j)) / (L(1:j-1, 1:j-1));
+    
+    z = inv((L(1:j-1, 1:j-1))) * (A(1:j-1,j));
+    U(1:j-1, j) = z;
+    
+    v(j:n) = A(j:n, j) - L(j:n, 1:j-1)*z
+  endif
+  
+  if(j<n)
+    L(j+1:n, j) = v(j+1:n) / v(j);
+  end
+  U(j,j) = v(j);
+  
+end
--- numerical1/task7.m
+++ numerical1/task7.m
@@ -1,1 +1,13 @@
 H = hilb(5)
+
+[L, U] = lu_fac(H)
+
+[n,n] = size(H);
+
+detH = 1;
+for i = 1:n
+  detH = detH * U(i,i);
+end
+
+detH
+det(H)
